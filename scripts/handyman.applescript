84c3e8a95dd6601702d754308fbc629375362a9c
alumbs <coolchibs@gmail.com>
2022-09-19 10:43:46 -0500

    update images

---
+++ scripts/handyman.applescript
@@ -0,0 +1,1 @@
+python3 /Users/alumbs/Documents/Projects/Kijiji-Repost-Headless/kijiji_repost_headless repost /Users/alumbs/Documents/Projects/Kijiji-Repost-Headless/Ads/Handyman1/item.yaml
---
+++ scripts/paycash.sh
@@ -0,0 +1,2 @@
+python3 /Users/alumbs/Documents/Projects/Kijiji-Repost-Headless/kijiji_repost_headless repost /Users/alumbs/Documents/Projects/Kijiji-Repost-Headless/Ads/PayCash/item.yaml
+
